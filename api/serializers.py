e0b28fc1f66b1ff58c2ef76d1ebdab0db87301b6
Rajuxz <Rajankhanalxz@gmail.com>
2023-04-14 17:14:18 +0545

    Making specific field readable

--- api/serializers.py
+++ api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from api.models import Company
 # Create serializers.
 class CompanySerializer(serializers.HyperlinkedModelSerializer):
+    company_id = serializers.ReadOnlyField()
     class Meta:
         model = Company
         fields = "__all__"
