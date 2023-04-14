b5720632dac18b3c8b0d439cf408f1e6550967de
Rajuxz <Rajankhanalxz@gmail.com>
2023-04-14 20:32:49 +0545

    Serializer for employee is set

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from api.models import Company
+from api.models import Company,Employee
 # Create serializers.
 class CompanySerializer(serializers.HyperlinkedModelSerializer):
     company_id = serializers.ReadOnlyField()
     class Meta:
         model = Company
+        fields = "__all__"
+
+
+class EmployeeSerialaizer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Employee
         fields = "__all__"
