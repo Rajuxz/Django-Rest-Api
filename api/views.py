d20d844ad8b5a12ec61112b57874bdb07716f646
Rajuxz <Rajankhanalxz@gmail.com>
2023-04-14 20:33:24 +0545

    View for employee API is registered

--- api/views.py
+++ api/views.py
@@ -1,8 +1,12 @@
 from rest_framework import viewsets
-from api.models import Company
-from api.serializers import  CompanySerializer
+from api.models import Company,Employee
+from api.serializers import  CompanySerializer,EmployeeSerialaizer
 
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
 
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerialaizer 
