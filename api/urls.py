43d7b41374dba89f6d694f1ee92403e36a178b23
Rajuxz <Rajankhanalxz@gmail.com>
2023-04-14 20:33:58 +0545

    Url for employee is set

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path,include
 from rest_framework import routers
-from api.views import CompanyViewSet
+from api.views import CompanyViewSet,EmployeeViewSet
 
 router = routers.DefaultRouter()
 router.register(r'companies', CompanyViewSet)
+router.register(r'employee',EmployeeViewSet)
 
 
 urlpatterns = [
-    path('', include(router.urls)),  
+    path('', include(router.urls)), 
 ]
