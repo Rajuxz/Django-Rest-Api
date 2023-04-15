48ff5be9858bd48062c29f8e348ebec188460579
Rajuxz <Rajankhanalxz@gmail.com>
2023-04-15 18:21:02 +0545

    Registering model to admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from api.models import Company,Employee
 
-# Register your models here.
+class CompanyAdmin(admin.ModelAdmin):
+    # company_id, company_name, company_location, about, company_type, is_active
+    list_display=['company_id','company_name','company_location','about','company_type','is_active',]
+
+class EmployeeAdmin(admin.ModelAdmin):
+     # company name email address phone about position
+    list_display=['name','email','address','phone','about','position',]
+
+admin.site.register(Company,CompanyAdmin)
+admin.site.register(Employee,EmployeeAdmin)
